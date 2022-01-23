b93103366259f78d20b6b474f4cae92eafe0d2ba
olgasenina <o.v.senina2020@gmail.com>
2022-01-23 18:04:48 +0300

    Добавьте файлы проекта.

---
+++ Module8_Сlear_and_info/FolderSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module8_Сlear_and_info
+{
+    public static class FolderSettings
+    {
+        /// <summary>
+        /// Считает размер папки на диске (вместе со всеми вложенными папками и файлами). 
+        /// На вход метод принимает URL директории, в ответ — размер в байтах.
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static long SizeFolder(DirectoryInfo d)
+        {
+            long size = 0;
+
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += SizeFolder(di);
+            }
+
+            return size;
+        }
+    }
+}
---
+++ Module8_Сlear_and_info/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Module8_Сlear_and_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите путь к каталогу ");
+
+            // Сохраним указанный путь
+            DirectoryInfo dirInfo = new DirectoryInfo(Console.ReadLine());
+
+            long SizeBefore = 0;    // размер до
+            long SizeAfter = 0;     // размер после
+
+            // Показать, сколько весит папка до очистки.
+            try
+            {
+                SizeBefore = FolderSettings.SizeFolder(dirInfo);
+                Console.WriteLine($"Исходный размер папки: {SizeBefore} байт");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Не удалось рассчитать исходный размер: {e.Message}");
+            }
+
+            // Выполнить очистку.
+            try
+            {
+                DirectoryWork.DeleteOldFiles(dirInfo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Не удалось очистить папку: {e.Message}");
+            }
+
+            // Показать, сколько папка весит после очистки.
+            try
+            {
+                SizeAfter = FolderSettings.SizeFolder(dirInfo);
+                Console.WriteLine($"Освобождено: {SizeBefore- SizeAfter}");
+                Console.WriteLine($"Текущий размер папки: {SizeAfter} байт");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Не удалось рассчитать текущий размер: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
